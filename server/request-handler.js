2dfd9b6964e8a2e1803c1c408b80fdbb10188caa
CZosa <zosachris@gmail.com>
2018-10-06 10:54:28 -0700

    Node Server is working. Complete

--- server/request-handler.js
+++ server/request-handler.js
@@ -19,12 +19,16 @@ this file and include it in basic-server.js so that it actually works.
 //   createdAt: null,
 // };
 
-var allChats = [];
+var allChats = [JSON.stringify({
+  'roomname': 'Lobby',
+  'username': 'Jack',
+  'text': 'Hello',
+})];
 
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
+  'access-control-allow-headers': 'content-type, accept, X-Parse-Application-Id, X-Parse-REST-API-Key',
   'access-control-max-age': 15 // Seconds.
 };
 
